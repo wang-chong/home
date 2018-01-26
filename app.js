2da9910679d19d7fa2c620b85c644e81b3b19522
wangchong <chong.wang@iqunxing.com>
2018-01-27 01:33:23 +0800

    hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  // res.send('hello world!')
+  res.json({
+    welcome: 'hello world!'
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
